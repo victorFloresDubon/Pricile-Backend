14edec787d6b3eca90171e65ca0d7d54506ea6c0
victorFloresDubon <vdubon@outlook.com>
2022-06-02 20:14:08 -0600

    Se agrega el método getConexion

--- connection/conexion.py
+++ connection/conexion.py
@@ -1,2 +1,20 @@
+from sqlite3 import DatabaseError
 import cx_oracle
 
+class ConexionOracle():
+
+    def getConexion(self):
+        user = 'CHATBOT'
+        password = 'CHATBOT'
+        host = '25.79.181.200'
+        port = 1521
+        dbName = ''
+        try:
+            url = f'{user}/{password}@{host}:{port}/{dbName}'
+            con = cx_oracle.connect(url)
+            print(con.version)
+        except cx_oracle.DatabaseError as e:
+            print("Ocurrió un problema al conectar", e)
+        
+        return con
+        
